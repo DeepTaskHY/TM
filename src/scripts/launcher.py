032d0e07cd96590c9c9d4edfdd4a4a61911b75f3
Eunsoo Lee <eunsoogi.lee@gmail.com>
2021-09-24 17:24:24 +0900

    Refactoring to python package

--- src/scripts/launcher.py
+++ src/scripts/launcher.py
@@ -5,11 +5,11 @@ from os import path
 from flask_socketio import SocketIO
 from flask import Flask, render_template
 from rosbridge import RosBridgeNamespace
-from helpers import get_test_configuration, timestamp
+from dtroslib.helpers import get_test_configuration, timestamp
 
 
-flask_configuration = get_test_configuration('flask')
-ros_configuration = get_test_configuration('ros')
+flask_configuration = get_test_configuration('tm', 'flask')
+ros_configuration = get_test_configuration('tm', 'ros')
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
